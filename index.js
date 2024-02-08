1728a3ace2eaaae3bdaa913b4241b9c6c6291232
GrupoSix <matheusess@hotmail.com>
2024-02-08 16:17:42 -0300

    teste de subidade codecommit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/checkEligibility', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}.`);
 });
 
 export default app;
